b10163b988519e97d6b7ec9d7085d7e10b2d705d
Gerson Sunyé <gerson.sunye@gmail.com>
2018-05-09 16:28:32 +0200

    package reorganization

---
+++ consistency-core/src/main/java/org/atlanmod/consistency/pubsub/Producer.java
@@ -0,0 +1,13 @@
+package org.atlanmod.consistency.pubsub;
+
+import java.io.Serializable;
+
+/**
+ * Created on 15/03/2017.
+ *
+ * @author AtlanMod team.
+ */
+public interface Producer {
+
+    void send(Serializable serializable);
+}
